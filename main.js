a47e8f693974e83f35b4a792e7b08a2a4fcbca3a
Mohamed Hamed <115209009+Mdhamedd@users.noreply.github.com>
2024-06-18 17:55:16 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+// const name = "Mohamed Hamed";
+// const age = 23;
+
+// console.log(`My name is ${name}, and My age is ${age}`);
+
+// let num = 10;
+// num++;
+// console.log(num);
+
+// console.log(+'5');
+// console.log(Number("5"));
+// console.log(parseInt("5"));
+// console.log(parseFloat("5.5"));
+
+// // Make Project To now what is this Zakah..
+
+// let zakah = window.prompt();
+
+// document.write(0.025 * zakah);
